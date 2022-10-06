45e5c373e46f63d0473295eeb932e6822c16ae0f
LuanEvander <luanevander.dev@gmail.com>
2022-10-06 20:53:22 -0300

    Criação de arquétipo Maven

---
+++ README.md
@@ -0,0 +1,3 @@
+# Introdução
+
+Esse algoritmo em linguagem Java foi desenvolvido para a disciplina de Resolução de Problemas II, do curso de Engenharia de Software da Universidade Federal do Pampa - Campus Alegrete.
---
+++ src/main/java/com/resolucaoproblemas/MissingPassengerException.java
@@ -0,0 +1,14 @@
+package com.resolucaoproblemas;
+
+
+public class MissingPassengerException extends RuntimeException {
+    private Vehicle vehicle;
+
+    public MissingPassengerException(Vehicle vehicle) {
+        super("Missing passenger at pickup location.");
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+}
